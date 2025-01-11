ed7a4e3e1c5ed7008d0e89b820132e05eefdcf54
Ashish Kumar chaurasia <ashishchaurasia811@gmail.com>
2025-01-12 01:31:35 +0530

    load balancer

---
+++ AccountMicroservice_2/Program.cs
@@ -0,0 +1,42 @@
+using AccountMicroservice_2;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+//app.MapControllers();
+var Summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+app.MapGet("/account", (HttpContext httpContext) =>
+{
+    var accounts2 = Enumerable.Range(1, 5).Select(index => new Account
+    {
+        Date = DateTime.Now.AddDays(index),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    })
+           .ToArray();
+    return accounts2;
+});
+
+app.Run();
